b686bd4075d634426d27acac24e9b52d6294be75
SelrahcD <c.desneuf@gmail.com>
2021-09-15 20:04:02 +0200

    Add first test

--- expensereport-php/ExpenseReportTest.php
+++ expensereport-php/ExpenseReportTest.php
@@ -8,6 +8,12 @@ class ExpenseReportTest extends TestCase
 {
     public function testExpenseReport(): void {
         $expenseReport = new ExpenseReport();
+        $expenses = [];
+        ob_start();
+        $expenseReport->print_report($expenses);
+        $result = ob_get_contents();
+        ob_clean();
+        \ApprovalTests\Approvals::verifyString($result);
     }
 
 }
