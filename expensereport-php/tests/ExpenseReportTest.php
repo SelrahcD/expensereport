da2abe28e4a930a0740f3c7342fc7d791a86b962
SelrahcD <c.desneuf@gmail.com>
2021-09-15 21:11:35 +0200

    Move tests

--- expensereport-php/tests/ExpenseReportTest.php
+++ expensereport-php/tests/ExpenseReportTest.php
@@ -3,8 +3,6 @@
 use ApprovalTests\Approvals;
 use PHPUnit\Framework\TestCase;
 
-require __DIR__ . '/vendor/autoload.php';
-
 class ExpenseReportTest extends TestCase
 {
     public function testExpenseReport(): void {
