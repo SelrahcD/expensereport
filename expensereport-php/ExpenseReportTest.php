ba81aae88bf33b4b4bbc94fc98b3ae637788e460
SelrahcD <c.desneuf@gmail.com>
2021-09-15 20:57:23 +0200

    Add test

--- expensereport-php/ExpenseReportTest.php
+++ expensereport-php/ExpenseReportTest.php
@@ -13,6 +13,7 @@ class ExpenseReportTest extends TestCase
             [new Expense(ExpenseType::DINNER, 12)],
             [new Expense(ExpenseType::BREAKFAST, 12)],
             [new Expense(ExpenseType::CAR_RENTAL, 12)],
+            [new Expense(ExpenseType::DINNER, 12), new Expense(ExpenseType::DINNER, 45)],
         ];
 
         $output = '';
