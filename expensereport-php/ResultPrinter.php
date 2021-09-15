4643fb205b0c8ba0e1670b5ffcda884548db4887
SelrahcD <c.desneuf@gmail.com>
2021-09-15 20:16:41 +0200

    Use a printer to stop being annoyed by dates

--- expensereport-php/ResultPrinter.php
+++ expensereport-php/ResultPrinter.php
@@ -10,6 +10,10 @@ final class ResultPrinter
 
     public function print(string $result): string
     {
-        return$result;
+        return preg_replace(
+            '/Expense Report (\d){4}-(\d){2}-(\d){2} (\d){2}:(\d){2}:(\d){2}[a|p]m/',
+            'Expense Report YYYY-MM-DD HH:MM:SSxm',
+            $result
+        );
     }
 }
