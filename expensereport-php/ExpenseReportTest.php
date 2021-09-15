99ee8d2dcc403069f6bd4b2c41133e76b906041b
SelrahcD <c.desneuf@gmail.com>
2021-09-15 19:58:39 +0200

    Run empty test

---
+++ expensereport-php/ExpenseReportTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class ExpenseReportTest extends TestCase
+{
+    public function testExpenseReport(): void {
+        
+    }
+
+}
